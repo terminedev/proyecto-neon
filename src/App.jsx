d5e9312e8bd27d63e816a678b970dfe661e4a30d
terminedev <terminegastondev@gmail.com>
2026-02-06 18:50:33 -0300

    Ruta de Paginas agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
 import { AuthProvider } from 'contexts/AuthProvider';
-import AddVideo from './components/private-content/AddVideo';
 import './App.css';
+import { Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <AuthProvider>
-
-
-
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
 
-          Rutas Públicas 
+          Rutas Públicas
           <Route path='/acceder' element={<PrivateAccess />} />
           <Route path='/acerca-de' element={<About />} />
 
           Rutas Protegidas
           <Route element={<ProtectedRoute />}>
-            <Route path='/' element={<Home />} />
+            <Route element={<Layout />}>
+              <Route path='/' element={<Home />} />
+              <Route path='/mis-playlists' element={<MyPlaylist />} />
+              <Route path='/mis-videos' element={<MyVideos />} />
+              <Route path='/playlist/:playlist_id' element={<PlaylistDetail />} />
+
+              <Route path='crear-playlist' element={<AddPlaylist />} />
+              <Route path='agregar-video' element={<AddVideo />} />
+
+              <Route path='/editar-playlist/:playlist_id' element={<EditPlaylist />} />
+              <Route path='/editar-video/:video_id' element={<EditVideo />} />
+
+            </Route>
           </Route>
 
           404
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
 
 
     </AuthProvider>
